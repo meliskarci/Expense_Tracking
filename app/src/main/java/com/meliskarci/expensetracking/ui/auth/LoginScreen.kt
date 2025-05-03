69cac0ac7cf5efb6b2e801f09946e02717c60b13
karci.meliss@gmail.com <karci.meliss@gmail.com>
2025-05-03 21:10:24 +0300

    commit Auth registerscreen 1 and loginscreen 2

--- app/src/main/java/com/meliskarci/expensetracking/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/meliskarci/expensetracking/ui/auth/LoginScreen.kt
@@ -1,26 +1,78 @@
 package com.meliskarci.expensetracking.ui.auth
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun RegisterScreen() {
+fun LoginScreen() {
+//    val view //12
+//    val isUser //14
+//    val issucces //19
+//    val mail = remember { mutableStateOf("") }
+//    val password = remember { mutableStateOf("") }
+//
+//    lanchedeffect{
+//
+//    } //14
+//
+//    Column(modd) {
+//
+//        Button() {
+//            is issuccess  //19
+//            Text()
+//        }
+//        TextButton() {
+//            Text()
+//        }
+//    }
+
+    /////////////////////////  2 ///////////////////////////
     val mail = remember { mutableStateOf("") }
     val password = remember { mutableStateOf("") }
 
-    Column(modd) {
 
-        Button() {
-            Text()
+    Column(modifier = Modifier.fillMaxSize().padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Login")
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mail.value,
+            onValueChange = { mail.value = it },
+            label = { Text("Mail") }
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = password.value,
+            onValueChange = { password.value = it },
+            label = { Text("Password") }
+        )
+        Button(modifier = Modifier.fillMaxWidth(),
+            onClick = {
+
+            }) {
+            Text(text = "Login")
         }
-        TextButton() {
-            Text()
+        TextButton(
+            onClick = { /* Navigate to RegisterScreen */
+            }
+        ) {
+            Text(text = "Register")
         }
     }
 }
--- app/src/main/java/com/meliskarci/expensetracking/ui/auth/RegisterScreen.kt
+++ app/src/main/java/com/meliskarci/expensetracking/ui/auth/RegisterScreen.kt
@@ -1,25 +1,73 @@
 package com.meliskarci.expensetracking.ui.auth
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 
 @Composable
 fun RegisterScreen() {
 
-    val viewModel //7
+//    val viewModel //7
+//
+//    is useraut //1*
+//    val mail = remember { mutableStateOf("") }
+//    val passwordConfirmation = remember { mutableStateOf("") }
+//
+////    launhhedeffect{
+////        if (isUser
+////            navcont)
+////    } //10
+//
+//    Column(modd) {
+//
+//        Button() {
+//            Text()
+//        }
+//    }
+//////////////////////////////////////////////////////////////  1   /////////////////////////////////////////////////////
     val mail = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
     val passwordConfirmation = remember { mutableStateOf("") }
 
-    Column(modd) {
-
-        Button() {
-            Text()
+    Column(modifier = Modifier.fillMaxSize().padding(15.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Register")
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mail.value,
+            onValueChange = { mail.value = it },
+            label = { Text("Mail") }
+        )
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = passwordConfirmation.value,
+            onValueChange = { passwordConfirmation.value = it },
+            label = { Text("Password") }
+        )
+        Button(modifier = Modifier.fillMaxWidth(),
+            onClick = {}) {
+            Text(text = "Register")
+        }
+        TextButton(
+            onClick = { /*Navigate to LoginScreen*/ }
+        ) {
+            Text(text = "Register")
         }
     }
 }
-// 1
+/////////////////////////      1           /////////////////////////
